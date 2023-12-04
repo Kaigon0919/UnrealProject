a79703d21e05fe076f09fa4f6de19a23ee154050
jh.park <jh.park@detailgames.com>
2023-12-04 22:26:09 +0900

    언리얼 프로젝트 생성, 새로운 레벨 생성

---
+++ TPSActionGame/Source/TPSActionGame/TPSActionGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSActionGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSACTIONGAME_API ATPSActionGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
